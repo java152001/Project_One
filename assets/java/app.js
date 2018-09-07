5970b6bfe0b8060b0c952fe876dda1246730d558
Anthony Demott <java152001@gmail.com>
2018-09-06 19:11:44 -0700

    Cleaned Up

---
+++ assets/java/app.js
@@ -0,0 +1,10 @@
+var ingredients = ["Beef", "Chicken", "Fish", "Carrots", "Mushroom", "Onion", "Milk", "Broth"];
+
+function addNew() {
+    var select = $("<select>");
+    var option = $("<option>");
+
+    for (var i = 0; i < ingredients.length; i++) {
+        
+    }
+}
