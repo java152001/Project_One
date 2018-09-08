90959d582a8abe9e3b79a61cc0492fd08217772c
Anthony Demott <java152001@gmail.com>
2018-09-07 23:12:17 -0700

    Working pantry list implemented

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,19 +1,51 @@
-var ingredients = ["Beef", "Chicken", "Fish", "Carrots", "Mushroom", "Onion", "Milk", "Broth"];
+var ingredients = ["--Meats--", "Beef", "Chicken", "Fish", "--Vegetables--", "Carrots", "Mushroom", "Onion", "Milk", "Broth"];
 var quantity = ["cups", "pounds", "grams"];
 var ingredientNumber = 0;
 
 var completedList = [];
 
 
-// addNew();
+// brings in list from local storage
+completedList = JSON.parse(localStorage.getItem("completedList"));
 
-completedList = JSON.parse(localStorage.getItem("todolist"));
+// checks to see if there is anything in the array and if so generate our current list
+if (completedList.length !== 0) {
+    createList();
+} 
 
-$("#try").on("click", function() {
+
+
+// generates a new selection entry on the page
+$("#add").on("click", function() {
     addNew();
+});
 
+// removes the select option when the X button is clicked
+$(document).on("click", "button.delete", function() {
+    var currentItem = $(this).attr('data-number');
+
+    $("#pantry").find("[data-ingredient-number='" + currentItem + "']").remove();
 
 });
+
+// removes from the current list and local storage
+$("#ingList").on("click", "button.listDelete", function() {
+    var currentDelete = $(this).attr('data-list-number');
+
+    completedList.splice(currentDelete, 1);
+
+    $("#ingList").empty();
+    $("#ingList").html("<h4>Your current list:</h4>");
+
+    createList();
+
+    localStorage.clear();
+
+    localStorage.setItem("completedList", JSON.stringify(completedList));
+})
+
+// tabulates all the current selections into an array and stores that array in local storage
+$("#submit").on("click", tabulate);
 
 function addNew() {
 
@@ -28,8 +60,15 @@ function addNew() {
     for (var i = 0; i < ingredients.length; i++) {
 
         newOption = $("<option>");
-        newOption.attr("value", ingredients[i]);
-        newOption.html(ingredients[i]);
+
+        // checks to see if the option should be a grouping or actual option
+        if (ingredients[i] !== "--Meats--" && ingredients[i] !== "--Vegetables--") {
+            newOption.attr("value", ingredients[i]);
+            newOption.html(ingredients[i]);
+        } else {
+            newOption = $("<optgroup>");
+            newOption.attr("label", ingredients[i]);
+        }
         newLine.append(newOption);
         newDiv.append(newLine);
         newDiv.attr("data-ingredient-number", ingredientNumber)
@@ -46,27 +85,42 @@ function addNew() {
     ingredientNumber++;
 }
 
-// removes the select option when the X button is clicked
-$(document).on("click", "button.delete", function() {
-    var currentItem = $(this).attr('data-number');
 
-    $("#pantry").find("[data-ingredient-number='" + currentItem + "']").remove();
-    
-});
-
-// tabulates all the current selections into an array and stores that array in local storage
-$("#submit").on("click", tabulate);
-
+// takes all of our existing select tags and combines them into an array of all selected options.
+// also checks to make sure that there aren't any duplicate selections.
 function tabulate() {
+
+    $("#ingList").empty();
+    $("#ingList").html("<h4>Your current list:</h4>");
+
     completedList = [];
 
     $("#pantry").find("select").each(function(index, select){
         completedList.push($(select).find(":selected").text())
     });
 
+    createList();
+
     localStorage.clear();
 
     localStorage.setItem("completedList", JSON.stringify(completedList));
 
-    console.log(completedList);
+
+}
+
+// takes our working list of items and creates a current ingredient list for the user to see.
+function createList() {
+
+    for (var i = 0; i < completedList.length; i++) {
+        var ingDiv = $("<div>");
+        var listDel = $("<button class='listDelete'>");
+
+        ingDiv.html(completedList[i]);
+        listDel.attr("data-list-number", i);
+        listDel.text("X");
+        ingDiv.append(listDel);
+        $("#ingList").append(ingDiv);
+    }
+
+    $("#ingList").css("visibility", "visible");
 }
